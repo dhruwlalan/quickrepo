429a575c884fbbcae23686c947137458097253cb
dhruw <dhruwlalan22@gmail.com>
2021-02-10 18:46:56 +0530

    feat: add register & check command for personal access tokens

--- index.js
+++ index.js
@@ -7,27 +7,37 @@ const github = require('./lib/github');
 
 program
    .command('init')
-   .description('Draw app baner')
-   .action(() => {
-      // process.stdout.write('\u001B[2J\u001B[0;0f');
-      console.log(chalk.cyanBright('quickrepo'));
+   .description('create repository')
+   .action(() => {});
+
+program
+   .command('register')
+   .description('register users personal access token')
+   .action(async () => {
+      const token = await github.setToken();
+      if (token) {
+         console.log(chalk.green.bold('stored token successfully!'));
+      } else {
+         console.log(chalk.red.bold('unable to store token!'));
+      }
    });
 
 program
-   .command('octocheck')
-   .description('check github user credentials')
+   .command('check')
+   .description('check for a valid token')
    .action(async () => {
-      const token = github.getStoredGithubToken();
+      const token = github.getToken();
       if (!token) {
-         const user = await github.registerToken();
-         console.log(user);
+         console.log('no token available, please register.');
       }
-      console.log(token);
+      const user = await github.checkToken(token);
+      console.log(chalk.white.bold('token\t-> ') + chalk.blue.bold(token));
+      console.log(chalk.white.bold('user\t-> ') + chalk.blue.bold(user.login));
+      console.log(chalk.green.bold('the stored token is valid!'));
    });
 
 program.version('1.0.0', '-v, --version', 'output the current version');
 program.parse(process.argv);
-
 if (!program.args.length) {
    program.help();
 }
