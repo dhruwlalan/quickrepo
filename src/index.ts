3d3fb834add25ebe9d65412315e8f3ed3abbcf0f
dhruw <dhruwlalan22@gmail.com>
2021-04-22 00:10:04 +0530

    chore: add prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-require('./utils/info');
+import './utils/info';
 
 import { program } from 'commander';
 import updateNotifier from 'update-notifier';
@@ -16,7 +16,9 @@ updateNotifier({
       version,
    },
    updateCheckInterval: 1000 * 60 * 60 * 24,
-}).notify({ isGlobal: true });
+}).notify({
+   isGlobal: true,
+});
 program.version(`${version}`, '-v, --version', 'output the current version');
 
 ///setup///
